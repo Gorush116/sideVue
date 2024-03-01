0a1530ab335e9a30c1db7fdfbecadb571735458a
Gorush116 <gorush34@gmail.com>
2024-03-01 23:36:43 +0900

    Initial commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+  
+  // 반응적인 상태의 속성
+  const count = ref(0);
+
+  // 속성 값을 변경하고 업데이트 할 수 있는 함수.
+  function increment() {
+    count.value++;
+  }
+
+  // ref 이해 시작
+  const roCount  = ref(2);
+
+  function readonly() {
+    console.log(roCount.value);
+
+    roCount.value++;
+  }
+
+  // ref 이해 끝
+
+  // 생명 주기 훅
+  onMounted(() => {
+    console.log(`숫자 세기의 초기값은 ${ count.value } 입니다.`);
+  })
+
+</script>
+
+
+<template>
+  <button @click="increment">숫자 세기: {{ count }}</button>
+
+  <button @click="readonly">읽기 전용 숫자: {{ roCount }}</button>
+
+  <button @click="writePossible">쓰기 가능 숫자: {{ wpCount }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ vue-project/src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>{{ msg }}</h1>
+    <h2>{{ msg2 }}</h2>
+    <h3 v-html="rawHtml"/>
+</template>
+
+<script>
+export default {
+    name: "",
+    props: {
+        msg: {
+            type: String,
+            required: true
+        }
+    },
+    mixins: [],
+    components: {},
+    data() {
+        return {
+            msg2: "나는 Vue다!",
+            rawHtml: "html태그 안이지롱",
+            bindId: "bid"
+        }
+    },
+    methods: {
+
+    },
+    created() {
+        
+    },
+    mounted() {
+
+    },
+    activated() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
